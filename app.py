0614dcf389199d15c3bb91c42edbc901c324af1b
Mohamed Abbas Moynudeen <abbas.m@CORP.SYSTECHMNG.NET>
2023-10-25 16:37:44 +0530

    bug fixer code added

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request
+import openai
+
+
+app = Flask(__name__)
+
+openai.api_key = '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'
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api", methods=["POST"])
+def api():
+    message = request.get_data(as_text=True)
+    print(message)
+    completion = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+        "role": "system",
+        "content": "You will be provided with a piece of Python code, and your task is to find and fix bugs in it."
+        },
+        {"role": "user", "content": message}
+    ]
+    )
+    print(completion.choices[0].message)
+    if completion.choices[0].message!=None:
+        return completion.choices[0].message['content']
+
+    else :
+        return 'Failed to Generate response!'
+    
+
+if __name__=='__main__':
+    app.run(debug=True)
+
